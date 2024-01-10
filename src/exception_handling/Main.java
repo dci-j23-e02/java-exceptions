b5bd7c9ace4869a25f6e4b8bb7a82325cd68b5c9
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-01-10 11:40:06 +0100

    Exception Handling First Example commit

--- src/exception_handling/Main.java
+++ src/exception_handling/Main.java
@@ -1,10 +1,24 @@
 package exception_handling;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
   public static void main(String[] args) {
-    ArrayList<Player> playerList =Player.getPlayers("src/exception_handling/players.dat");
+
+    try {
+      ArrayList<Player> playerList =Player.getPlayers("players.dat");
+      playerList.forEach(
+          player -> System.out.println("Player Name: " + player.getName()
+          + "\n"+ "Player Score: " + player.getScore())
+      );
+    }catch (Exception e) {
+    System.out.println("The specified fila path was not found: "+ e.getMessage());
+   /*   StackTraceElement[] stackTrace = e.getStackTrace();
+      for (StackTraceElement element :stackTrace ) {
+        System.out.println(element.toString());
+      }*/
+    }
   }
 }
